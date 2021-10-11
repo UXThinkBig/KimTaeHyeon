d7abe9b3a3bdaa9d61258328d01df2c3e3e24968
김태현 <taehyk528@gmail.com>
2021-10-11 20:57:52 +0900

    ➕[ADD] : UsageScene 생성 (#3) - Usage Storyboard - Usage ViewController - 시작점 지정 - Large Title 적용

---
+++ UXTTextField/UXTTextField/Demo/Scene/Usage/UsageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UsageViewController.swift
+//  UXTTextField
+//
+//  Created by taehy.k on 2021/10/11.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class UsageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAttributes()
+    }
+    
+    private func setupAttributes() {
+    }
+}
